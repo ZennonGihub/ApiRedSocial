f5db2e9bd4a548cd9c1b07e8969c3ede52354e80
Zennon <zeitaa34@gmail.com>
2025-12-01 13:24:11 -0300

    Creacion y configuracion de yaml

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 const errors = require("../main/Response/errors.js");
 const cookieParser = require("cookie-parser");
 const routerApi = require("../main/router/index");
@@ -21,6 +24,10 @@ app.use(passport.initialize());
 app.use(logsErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
+
+const swaggerDocument = YAML.load(path.join(__dirname, "../main/openapi.yaml"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /*
 app.listen(3000, () => {
